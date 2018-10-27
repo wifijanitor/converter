21e5fae7d1bbf9f1f5d5a697377d475af88ea7c4
wifijanitor <steve@wifijanitor.com>
2018-10-27 16:47:53 -0500

    changed to argparse from getopt, added functions per converstion type

--- converter.py
+++ converter.py
@@ -1,36 +1,7 @@
 #!/usr/bin/env python3
 
-'''
-Usage:
-  This script will attempt to find files based on
-  user determined location and size.
-
-  Currently this only works for MP4 and MKV extensions, I'll add in
-  AVI functionality at some point.
-
-  If the files are on a remote drive, it will copy the files
-  to a local folder and use ffmpeg to transcode and make the files smaller.
-
-  The profile is will transcode up to a 1080p file
-  using X.265 and aac encoding.
-
-  If that completes, it will:
-  remove the old fiile
-  move the new file back to the original location.
-
-  Input options:
-
-    -d/--directory      Base seach location
-    -s/--size           File size you want to search for G assumed
-    -h/--help           Display this help and exit
-    -v/--ver/--version  Dislpays script verision
-
-  converter.py -d /Volumes/TV Shows/ -s 11
-'''
-
 import os
-import sys
-import getopt
+import argparse
 import glob
 import subprocess
 import shutil
@@ -45,61 +16,37 @@ logging.basicConfig(
 
 logger = logging.getLogger(__name__)
 
-version = 2.0
-ver = sys.version_info[0] > 2
-
 directory = None
 size = None
-bsize = None
 
 org = expanduser('~/Converter/original/')
 conv = expanduser('~/Converter/modified/')
 found = expanduser('~/Converter/found.txt')
 
 
-class Usage(Exception):
-    def __init__(self, msg):
-        self.msg = msg
-
-
-def usage(msg=None):
-    logging.info('we hit the def usage function')
-    print(__doc__)
-    if msg:
-        print(msg)
-
-
-def parseOptions(argv):
+def parseOptions():
     global directory
     global size
-    while True:
-        try:
-            try:
-                opts, args = getopt.getopt(
-                    argv[1:], 'hvd:s:', ['size=', 'version', 'help', 'directory='])
-            except getopt.error as exc:
-                return usage('Error: {}'.format(str(exc)))
-            for o, a in opts:
-                if o in ('-h', '--help'):
-                    usage()
-                    return 0
-                elif o in ('-v', '--version'):
-                    return usage('Transcoder Version: {}'.format(version))
-                elif o in ('-d', '--directory'):
-                    directory = str(a)
-                elif o in ('-s', '--size'):
-                    size = int(a) << 30
-                else:
-                    return usage('Error: Unknown option, exiting...')
-            if not directory:
-                return usage('Error: Base search directory \
-                    (-d/--directory) not provided, exiting.')
-            if not size:
-                return usage('Error: File size \
-                    (-s/--size) not provided, exiting.')
-        except Exception as exc:
-            return usage('Error:{}'.format(str(exc)))
-        return 1
+    parser = argparse.ArgumentParser(
+        prog='Converter',
+        formatter_class=argparse.RawDescriptionHelpFormatter,
+        description='''
+            Convert files to MKV using X265 and AAC encodings
+  ''')
+    parser.add_argument('-d', '--directory',
+                        metavar='Directory',
+                        help="Base search location",
+                        required=True)
+    parser.add_argument('-s', '--size',
+                        metavar='Size',
+                        help="file size you want to search for",
+                        type=int, required=True)
+    parser.add_argument('-v', '--version',
+                        action='version',
+                        version='%(prog)s 3.0')
+    args = parser.parse_args()
+    directory = str(args.directory)
+    size = args.size
 
 
 def path_exists():
@@ -127,7 +74,7 @@ def find_files():
     os.chdir(directory)
     with open(found, 'wt') as file:
         logging.info('Finding files')
-        for f in glob.iglob('**/*.m*', recursive=True):
+        for f in glob.iglob('**/*.**', recursive=True):
             file_size = os.path.getsize(f)
             if file_size >= size:
                 file.write(f + '\n')
@@ -135,7 +82,7 @@ def find_files():
                 continue
 
 
-def convert():
+def convert_main():
     '''
     converts files to X265 video and AAC audio codecs
     '''
@@ -143,30 +90,97 @@ def convert():
     logging.info("Starting to convert ")
     with open(found, 'rt') as shows:
         for row in shows:
-            line = row.rstrip().split('/')
-            print('This is the file we are moving')
-            if 'mkv' in line[0] or 'mp4' in line[0]:
-                file = line[0]
-                logging.info('Moving ' + file + ' for conversion')
-                shutil.move(directory + '/' + file, org)
-                logging.info('Converting ' + file)
-                subprocess.run(
-                    'ffmpeg -sn -i ' + '"' + file + '"' +
-                    ' -c:v libx265 -crf 28 -c:a libfdk_aac -b:a  128k ' +
-                    conv + '"' + file + '"' + ' -hide_banner', shell=True
-                )
-                logging.info('Done converting' + line[0])
-            elif 'mkv' in line[1] or 'mp4' in line[1]:
-                file = line[1]
-                folder = line[0]
-                logging.info('Moving ' + file + ' for conversion')
-                shutil.move(directory + '/' + folder + '/' + file, org)
-                logging.info('Converting ' + file)
-                subprocess.run(
-                    'ffmpeg -sn -i ' + '"' + file + '"' +
-                    ' -c:v libx265 -crf 28 -c:a aac -b:a 128k ' +
-                    conv + '"' + file + '"' + ' -hide_banner', shell=True
-                )
+            movie, ext = row.rstrip().split('.')
+            if 'mkv' in ext:
+                convert_mkv(row)
+            elif 'mp4' in ext:
+                convert_mp4(row)
+            elif 'avi' in ext:
+                convert_avi(row)
+
+
+def convert_mkv(row):
+    line = row.rstrip().split('/')
+    if 'mkv' in line[0]:
+        file = line[0]
+        print(f'Converting {file}')
+        logging.info(f'Moving {file} for conversion')
+        shutil.move(directory + '/' + file, org)
+        logging.info(f'Converting {file}')
+        subprocess.run(
+            'ffmpeg -sn -i ' + '"' + file + '"' +
+            ' -c:v libx265 -crf 28 -c:a libfdk_aac -b:a  128k ' +
+            conv + '"' + file + '"' + ' -hide_banner', shell=True
+        )
+        logging.info(f'Done converting {file}')
+    elif 'mkv' in line[1]:
+        file = line[1]
+        folder = line[0]
+        logging.info(f'Moving {file} for conversion')
+        shutil.move(directory + '/' + folder + '/' + file, org)
+        logging.info(f'Converting {file}')
+        subprocess.run(
+            'ffmpeg -sn -i ' + '"' + file + '"' +
+            ' -c:v libx265 -crf 28 -c:a aac -b:a 128k ' +
+            conv + '"' + file + '"' + ' -hide_banner', shell=True
+        )
+    return
+
+
+def convert_mp4(row):
+    line = row.rstrip().split('/')
+    if 'mp4' in line[0]:
+        old, ext = line[0].split('.')
+        file = line[0]
+        logging.info(f'Moving {file} for conversion')
+        shutil.move(directory + '/' + file, org)
+        logging.info(f'Converting {file}')
+        subprocess.run(
+            'ffmpeg -sn -i ' + '"' + file + '"' +
+            ' -c:v libx265 -crf 28 -c:a libfdk_aac -b:a  128k ' +
+            conv + '"' + old + '.mkv' + '"' + ' -hide_banner',
+            shell=True)
+        logging.info(f'Done converting {file}')
+    elif 'mp4' in line[1]:
+        old, ext = line[1].split('.')
+        file = line[1]
+        folder = line[0]
+        shutil.move(directory + '/' + folder + '/' + file, org)
+        logging.info(f'Converting {file}')
+        subprocess.run(
+            'ffmpeg -sn -i ' + '"' + file + '"' +
+            ' -c:v libx265 -crf 28 -c:a libfdk_aac -b:a  128k ' +
+            conv + '"' + old + '.mkv' + '"' + ' -hide_banner',
+            shell=True)
+    return
+
+
+def convert_avi(row):
+    line = row.rstrip().split('/')
+    if 'avi' in line[0]:
+        old, ext = line[0].split('.')
+        file = line[0]
+        logging.info(f'Moving {file} for conversion')
+        shutil.move(directory + '/' + file, org)
+        logging.info(f'Converting {file}')
+        subprocess.run(
+            'ffmpeg -sn -i ' + '"' + file + '"' +
+            ' -c:v libx265 -crf 28 -c:a libfdk_aac -b:a  128k ' +
+            conv + '"' + old + '.mkv' + '"' + ' -hide_banner',
+            shell=True)
+        logging.info(f'Done converting {file}')
+    elif 'avi' in line[1]:
+        old, ext = line[1].split('.')
+        file = line[1]
+        folder = line[0]
+        shutil.move(directory + '/' + folder + '/' + file, org)
+        logging.info(f'Converting {file}')
+        subprocess.run(
+            'ffmpeg -sn -i ' + '"' + file + '"' +
+            ' -c:v libx265 -crf 28 -c:a libfdk_aac -b:a  128k ' +
+            conv + '"' + old + '.mkv' + '"' + ' -hide_banner',
+            shell=True)
+    return
 
 
 def move_conv():
@@ -180,29 +194,39 @@ def move_conv():
             line = row.rstrip().split('/')
             if 'mkv' in line[0]:
                 file = line[0]
-                logging.info('Moving ' + file +
-                             " back to it's original location")
-                shutil.move(file, directory + '/' + file)
+                logging.info(f"Moving {file} back to it's original location")
+                shutil.move(file, directory)
             elif 'mp4' in line[0]:
+                old, ext = line[0].split('.')
                 file = line[0]
-                logging.info('Moving ' + file +
-                             " back to it's original location")
-                shutil.move(file, directory + '/' +
-                            os.path.splitext(file)[0] + '.mkv')
+                logging.info(f"Moving {file} back to it's original location")
+                shutil.move(old + ".mkv", directory)
+            elif 'avi' in line[0]:
+                old, ext = line[0].split('.')
+                file = line[0]
+                logging.info(f"Moving {file} back to it's original location")
+                shutil.move(old + ".mkv", directory)
             elif 'mkv' in line[1]:
                 file = line[1]
                 folder = line[0]
                 logging.info('Moving ' + file +
                              " back to it's orignial location")
                 shutil.move(file, directory + '/' +
-                            folder + '/' + file)
+                            folder)
             elif 'mp4' in line[1]:
+                old, ext = line[1].split('.')
                 file = line[1]
                 folder = line[0]
-                logging.info('Moving ' + file +
-                             " back to it's orignial location")
-                shutil.move(file, directory + '/' +
-                            folder + '/' + os.path.splitext(file)[0] + '.mkv')
+                logging.info(f"Moving {file} back to it's orignial location")
+                shutil.move(old + ".mkv", directory + '/' +
+                            folder)
+            elif 'avi' in line[1]:
+                old, ext = line[1].split('.')
+                file = line[1]
+                folder = line[0]
+                logging.info(f"Moving {file} back to it's orignial location")
+                shutil.move(old + ".mkv", directory + '/' +
+                            folder)
 
 
 def clean_up():
@@ -213,30 +237,18 @@ def clean_up():
     logging.info("Cleaning up")
     with open(found, 'rt') as shows:
         for row in shows:
-            line = row.rstrip()
+            *junk, line = row.rstrip().split('/')
             os.remove(line)
 
 
-def main(argv=None):
-    if len(sys.argv) <= 1:
-        return usage('No options were provided, plrease refer to usage')
-    if argv is None:
-        argv = sys.argv
-        init = parseOptions(argv)
-        logging.info(init)
-        if init == 0:
-            return 0
-        elif init == 1:
-            path_exists()
-            find_files()
-            convert()
-            move_conv()
-            clean_up()
+def main():
+    parseOptions()
+    path_exists()
+    find_files()
+    convert_main()
+    move_conv()
+    clean_up()
 
 
 if __name__ == '__main__':
-    try:
-        sys.exit(main())
-        print('Exiting')
-    except KeyboardInterrupt as exc:
-        sys.exit('User cancelled, exiting.')
+    main()
